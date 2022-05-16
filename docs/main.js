9a084e8f0585efecf00da5b3d80ecac64566501d
michael28400 <poirier.mi@free.fr>
2022-05-16 15:08:43 +0200

    nouvelle modification

---
+++ docs/main.js
@@ -0,0 +1,12 @@
+
+// navigation 
+const editNav = () => {
+    var x = document.getElementById("myTopnav");
+    if(x.className === "topnav") {
+        x.className += " responsive";
+    }else{
+        x.className = "topnav";
+    }
+}
+
+
